c864a5d831db4f8b4ae6d41dbb157f4e37ff72f6
andremazevedo <andremazevedo6@gmail.com>
2022-06-10 17:18:20 +0100

    No boardboard no mo

--- takuzu.py
+++ takuzu.py
@@ -32,48 +32,49 @@ class TakuzuState:
 
     # TODO: outros metodos da classe
 
+    def get_board(self):
+        """Devolve a representação interna do tabuleiro associada ao Estado."""
+        return self.board
+
+
+
 
 class Board:
     """Representação interna de um tabuleiro de Takuzu."""
 
     N = None
 
-    def __init__(self, board):
-        self.board = board
+    def __init__(self, matrix):
+        self.matrix = matrix
 
     def __str__(self):
-        return str(self.board).replace(' [', '').replace('[', '').replace(']', '')
+        return str(self.matrix).replace(' [', '').replace('[', '').replace(']', '')
 
     def get_number(self, row: int, col: int) -> int:
         """Devolve o valor na respetiva posição do tabuleiro."""
-        # TODO
-        return self.board[row, col]
+        return self.matrix[row, col]
 
     def adjacent_vertical_numbers(self, row: int, col: int) -> (int, int):
         """Devolve os valores imediatamente abaixo e acima,
         respectivamente."""
-
-        below = None
+        below, above = None, None
         if row < Board.N - 1:
-            below = self.board[row + 1, col]
+            below = self.matrix[row + 1, col]
 
-        above = None
         if row > 0:
-            above = self.board[row - 1, col]
+            above = self.matrix[row - 1, col]
 
         return (below, above)
 
     def adjacent_horizontal_numbers(self, row: int, col: int) -> (int, int):
         """Devolve os valores imediatamente à esquerda e à direita,
         respectivamente."""
-
-        left = None
+        left, right = None, None
         if col > 0:
-            left = self.board[row, col - 1]
+            left = self.matrix[row, col - 1]
 
-        right = None
         if col < Board.N - 1:
-            right = self.board[row, col + 1]
+            right = self.matrix[row, col + 1]
 
         return (left, right)
 
@@ -89,15 +90,59 @@ class Board:
             > stdin.readline()
         """
         Board.N = int(stdin.readline())
-        board = np.empty((0, Board.N), int)
+        matrix = np.empty((0, Board.N), int)
 
         for i in range(Board.N):
             int_list = [ int(x) for x in stdin.readline().split('\t')] # i.e [2, 1, 2, 0]
-            board = np.append(board, np.array([int_list]), axis=0)
+            matrix = np.append(matrix, np.array([int_list]), axis=0)
 
-        return Board(board)
+        return Board(matrix)
 
     # TODO: outros metodos da classe
+
+    def domain(self, row: int, col: int):
+        domain = [0, 1]
+        for number in domain:
+
+            below, above = self.adjacent_vertical_numbers(row, col)
+            left, right = self.adjacent_horizontal_numbers(row, col)
+            if left == number == right or below == number == above:
+                domain.remove(number)
+                break
+
+            n_numbers_row = np.count_nonzero(self.matrix==number, axis=1)[row]
+            n_numbers_col = np.count_nonzero(self.matrix==number, axis=0)[col]
+            if n_numbers_row >= Board.N/2 or n_numbers_col >= Board.N/2:
+                domain.remove(number)
+                break
+
+        return domain
+
+    def result(self, action):
+        left, right, number = action
+        result_matrix = self.matrix.copy()
+        result_matrix[left, right] = number
+        return Board(result_matrix)
+
+    def occurrences_test(self) -> bool:
+        """Devolve true se o numero de 0s e 1s for igual para cada linha
+        e coluna."""
+        n_zeros_rows = np.count_nonzero(self.matrix==0, axis=1)#[1 1 1 1]
+        n_ones_rows = np.count_nonzero(self.matrix==1, axis=1)#[1 0 0 2]
+
+        n_zeros_cols = np.count_nonzero(self.matrix==0, axis=0)#[0 1 1 2]
+        n_ones_cols = np.count_nonzero(self.matrix==1, axis=0)#[1 2 0 0]
+
+        return np.array_equal(n_zeros_rows, n_ones_rows) and np.array_equal(n_zeros_cols, n_ones_cols)
+
+    def unique_test(self) -> bool:
+        """Devolve true se todas as linhas sao diferentes e se todas as
+        colunas sao diferentes"""
+        n_different_rows = np.unique(self.matrix, axis=0).shape[0]
+        n_different_cols = np.unique(self.matrix.T, axis=0).shape[0]
+
+        return n_different_rows == n_different_cols == Board.N
+
 
 
 class Takuzu(Problem):
@@ -111,7 +156,18 @@ class Takuzu(Problem):
         """Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento."""
         # TODO
-        pass
+        board = state.get_board()
+        possible_actions = []
+        for row in range(Board.N):
+            for col in range(Board.N):
+                if board.get_number(row, col) == 2:
+                    for number in board.domain(row, col):
+                        possible_actions.append((row, col, number))
+
+        # print(possible_actions)
+        # quit()
+
+        return possible_actions
 
     def result(self, state: TakuzuState, action):
         """Retorna o estado resultante de executar a 'action' sobre
@@ -119,37 +175,29 @@ class Takuzu(Problem):
         das presentes na lista obtida pela execução de
         self.actions(state)."""
         # TODO
-        board = state.board.board.copy()
-        board[action[0], action[1]] = action[2]
-        return TakuzuState(Board(board))
+        result_board = state.get_board().result(action)
+        return TakuzuState(result_board)
 
     def goal_test(self, state: TakuzuState):
         """Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se todas as posições do tabuleiro
         estão preenchidas com uma sequência de números adjacentes."""
         # TODO
-        board = state.board
-
-        # if len(np.where(board == 2)) == 0:
-        #     return False
+        board = state.get_board()
 
         for row in range(Board.N):
             for col in range(Board.N):
                 number = board.get_number(row, col)
-                if number == 2:
+                below, above = board.adjacent_vertical_numbers(row, col)
+                left, right = board.adjacent_horizontal_numbers(row, col)
+                if number == 2 or below == number == above or left == number == right:
                     return False
+    
+        if not board.occurrences_test():
+            return False
 
-        for col in range(Board.N):
-            for row in range(Board.N):
-                (left, right) = board.adjacent_horizontal_numbers(row, col)
-                if (board.get_number(row, col) == left and left == right):
-                    return False
-
-        for row in range(Board.N):
-            for col in range(Board.N):
-                (below, above) = board.adjacent_vertical_numbers(row, col)
-                if (board.get_number(row, col) == below and below == above):
-                    return False
+        if not board.unique_test():
+            return False
 
         return True
 
@@ -175,21 +223,9 @@ if __name__ == "__main__":
     # Criar uma instância de Takuzu:
     problem = Takuzu(board)
 
-    # Criar um estado com a configuração inicial:
-    s0 = TakuzuState(board)
-    print("Initial:\n", s0.board, sep="")
-
-    # Aplicar as ações que resolvem a instância
-    s1 = problem.result(s0, (0, 0, 0))
-    s2 = problem.result(s1, (0, 2, 1))
-    s3 = problem.result(s2, (1, 0, 1))
-    s4 = problem.result(s3, (1, 1, 0))
-    s5 = problem.result(s4, (1, 3, 1))
-    s6 = problem.result(s5, (2, 0, 0))
-    s7 = problem.result(s6, (2, 2, 1))
-    s8 = problem.result(s7, (2, 3, 1))
-    s9 = problem.result(s8, (3, 2, 0))
+    # Obter o nó solução usando a procura em profundidade:
+    goal_node = depth_first_tree_search(problem)
 
     # Verificar se foi atingida a solução
-    print("Is goal?", problem.goal_test(s9))
-    print("Solution:\n", s9.board, sep="")
+    print("Is goal?", problem.goal_test(goal_node.state))
+    print("Solution:\n", goal_node.state.board, sep="")
